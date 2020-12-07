73638bce8f6e9c67455214ab3b2bef1e607d9479
yaojiankang <814257280@qq.com>
2020-12-07 16:29:48 +0800

    two

--- yaojk-eureka-client-provide/src/main/java/com/eureka/client/payment/provide/EurekaProvideApplication.java
+++ yaojk-eureka-client-provide/src/main/java/com/eureka/client/payment/provide/EurekaProvideApplication.java
@@ -3,6 +3,7 @@ package com.eureka.client.payment.provide;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan(basePackages = "com.eureka.client.payment.provide.dao")
 public class EurekaProvideApplication {
 
     public static void main(String[] args) {
--- yaojk-eureka-client-provide/src/main/java/com/eureka/client/payment/provide/web/controller/UserController.java
+++ yaojk-eureka-client-provide/src/main/java/com/eureka/client/payment/provide/web/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.eureka.client.payment.provide.controller;
+package com.eureka.client.payment.provide.web.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
